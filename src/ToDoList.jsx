f60cecfaeb269b4a2e5904ce0235adacdaa6c6c8
Rashmi <5Rashmi>
2024-08-27 22:42:38 +0530

    added functionalities

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,21 +1,41 @@
 import React, {useState} from "react";
 
 function ToDoList(){
-    const [tasks, setTask] = useState(["Eat", "Sleep", "Write"]);
+    const [tasks, setTask] = useState([]);
+    const [editIndex, setEditIndex] = useState(null);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
         setNewTask(event.target.value);
     }
     function addTask(){
-        
+        if(newTask.trim() !== ""){
+            setTask(t => [...t, {text: newTask, completed:false}]);
+            setNewTask("");
+        }
     }
     function editTask(index){
-
+        setEditIndex(index);
+        setNewTask(tasks[index].text);
+    }
+    function saveTask(index){
+        const editedTasks = [...tasks];
+        editedTasks[index].text = newTask;
+        setTask(editedTasks);
+        setEditIndex(null);
+        setNewTask('');
     }
     function deleteTask(index){
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTask(updatedTasks);
     }
+    function toggleTaskCompletion(index){
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
+        setTask(updatedTasks);
+    }
+
+    const completedTasks = tasks.sort((a, b) => a.completed - b.completed);
 
     return(
         <div>
@@ -26,13 +46,21 @@ function ToDoList(){
             </div>
             <div className="taskAdded">
                 <ul>
-                    {tasks.map((task, index) =>
+                    {completedTasks.map((task, index) =>
                     <li key={index}>
                         <div className="todo">
-                            <input type="checkbox" />
-                            <label className="todo-label">{task}</label>
-                            <button className="editBtn"
-                                onClick={() => editTask(index)}>✏️</button>
+                            <input type="checkbox"
+                            checked = {task.completed}
+                            onChange={() => toggleTaskCompletion(index)}/>
+
+                            <label className="todo-label"
+                            style={{textDecoration: task.completed ? "line-through" : "none"}}>
+                                {task.text}</label>
+
+                            {editIndex === index ? (<button className="saveBtn"
+                                onClick={() => saveTask(index)}>💾</button>) :
+                                (<button className="editBtn"
+                                    onClick={() => editTask(index)}>✏️</button>)}
                             <button className="deleteBtn" 
                                 onClick={() => deleteTask(index)}>🗑️</button>
                         </div>
